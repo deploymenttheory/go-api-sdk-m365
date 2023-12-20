9202dbf247abf6f3651e90e1b983c49e87ac04fe
ShocOne <62835948+ShocOne@users.noreply.github.com>
2023-12-20 15:57:17 +0000

    added examples

---
+++ examples/intune/device_management_script_group_assignments/CreateDeviceManagementScriptGroupAssignmentByScriptIDAndGroupID/CreateDeviceManagementScriptGroupAssignmentByScriptIDAndGroupID.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/deploymenttheory/go-api-sdk-m365/sdk/http_client" // Import http_client for logging
+	intuneSDK "github.com/deploymenttheory/go-api-sdk-m365/sdk/m365/intune"
+)
+
+func main() {
+	// Define the path to the JSON configuration file
+	configFilePath := "/Users/dafyddwatkins/GitHub/deploymenttheory/go-api-sdk-m365/clientauth.json"
+
+	// Load the client OAuth credentials from the configuration file
+	clientAuthConfig, err := http_client.LoadClientAuthConfig(configFilePath)
+	if err != nil {
+		log.Fatalf("Failed to load client OAuth configuration: %v", err)
+	}
+
+	// Instantiate the default logger and set the desired log level
+	logger := http_client.NewDefaultLogger()
+	logger.SetLevel(http_client.LogLevelDebug) // Adjust the log level as needed
+
+	// Configuration for the HTTP client
+	httpClientconfig := http_client.Config{
+		LogLevel:                  http_client.LogLevelDebug,
+		MaxRetryAttempts:          3,
+		EnableDynamicRateLimiting: true,
+		Logger:                    logger,
+		MaxConcurrentRequests:     5,
+	}
+
+	// initialize HTTP client instance
+	httpClient, err := http_client.NewClient(httpClientconfig, clientAuthConfig, logger)
+	if err != nil {
+		log.Fatalf("Failed to create HTTP client: %v", err)
+	}
+
+	// Create an Intune client with the HTTP client
+	intune := &intuneSDK.Client{HTTP: httpClient}
+
+	// Example script ID and group ID
+	scriptID := "18f676c3-5835-4f9c-9437-438652a3ef76" // Replace with actual script ID
+	groupID := "4f03648f-2a22-4aa2-8131-fb22eea9db3c"  // Replace with actual group ID
+
+	// Create a device management script group assignment
+	response, err := intune.CreateDeviceManagementScriptGroupAssignmentByScriptIDAndGroupID(scriptID, groupID)
+	if err != nil {
+		log.Fatalf("Failed to create device management script group assignment: %v", err)
+	}
+
+	// Pretty print the response
+	jsonData, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal response: %v", err)
+	}
+	fmt.Println("Created Group Assignment:")
+	fmt.Println(string(jsonData))
+}
