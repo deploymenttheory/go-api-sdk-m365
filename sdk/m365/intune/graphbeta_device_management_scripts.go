96305086fa0eed4f4fbe4e006d07bacce8a2c19f
ShocOne <62835948+ShocOne@users.noreply.github.com>
2023-12-20 15:54:13 +0000

    bug fixing

--- sdk/m365/intune/graphbeta_device_management_scripts.go
+++ sdk/m365/intune/graphbeta_device_management_scripts.go
@@ -1,5 +1,5 @@
 // graphbeta_device_management_scripts.go
-// Graph Beta Api - Intune: Proactive Remediation
+// Graph Beta Api - Intune: Windows Scripts
 // Documentation: https://learn.microsoft.com/en-us/mem/intune/fundamentals/remediations
 // Intune location: https://intune.microsoft.com/#view/Microsoft_Intune_DeviceSettings/DevicesMenu/~/remediations
 // API reference: https://learn.microsoft.com/en-us/graph/api/resources/intune-shared-devicemanagementscript?view=graph-rest-beta
@@ -8,15 +8,13 @@
 package intune
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"time"
 
 	shared "github.com/deploymenttheory/go-api-sdk-m365/sdk/shared"
 )
 
-const urideviceManagementScripts = "/beta/deviceManagement/deviceManagementScripts"
+const uriBetaDeviceManagementScripts = "/beta/deviceManagement/deviceManagementScripts"
 
 // Struct for a list of Device Management Scripts
 type ResourceDeviceManagementScriptsList struct {
@@ -67,34 +65,9 @@ type ResourceDeviceManagementScriptRequest struct {
 	RunAs32Bit            bool     `json:"runAs32Bit"`
 }
 
-// ResourceDeviceManagementScriptAssign represents the structure for assigning a device management script.
-type ResourceDeviceManagementScriptAssign struct {
-	DeviceManagementScriptGroupAssignments []DeviceManagementScriptGroupAssignment `json:"deviceManagementScriptGroupAssignments"`
-	DeviceManagementScriptAssignments      []DeviceManagementScriptAssignment      `json:"deviceManagementScriptAssignments"`
-}
-
-// DeviceManagementScriptGroupAssignment represents a group assignment for a device management script.
-type DeviceManagementScriptGroupAssignment struct {
-	OdataType     string `json:"@odata.type"`
-	ID            string `json:"id"`
-	TargetGroupID string `json:"targetGroupId"`
-}
-
-// DeviceManagementScriptAssignment represents a script assignment.
-type DeviceManagementScriptAssignment struct {
-	OdataType string           `json:"@odata.type"`
-	ID        string           `json:"id"`
-	Target    AssignmentTarget `json:"target"`
-}
-
-// AssignmentTarget represents the target for an assignment.
-type AssignmentTarget struct {
-	OdataType string `json:"@odata.type"`
-}
-
 // GetDeviceManagementScripts gets a list of all Intune Device Management Scripts
 func (c *Client) GetDeviceManagementScripts() (*ResourceDeviceManagementScriptsList, error) {
-	endpoint := urideviceManagementScripts
+	endpoint := uriBetaDeviceManagementScripts
 
 	var deviceManagementScripts ResourceDeviceManagementScriptsList
 	resp, err := c.HTTP.DoRequest("GET", endpoint, nil, &deviceManagementScripts)
@@ -112,7 +85,7 @@ func (c *Client) GetDeviceManagementScripts() (*ResourceDeviceManagementScriptsL
 
 // GetDeviceManagementScriptByID retrieves a Device Management Script by its ID.
 func (c *Client) GetDeviceManagementScriptByID(id string) (*ResourceDeviceManagementScript, error) {
-	endpoint := fmt.Sprintf("%s/%s", urideviceManagementScripts, id)
+	endpoint := fmt.Sprintf("%s/%s", uriBetaDeviceManagementScripts, id)
 
 	var deviceManagementScript ResourceDeviceManagementScript
 	resp, err := c.HTTP.DoRequest("GET", endpoint, nil, &deviceManagementScript)
@@ -151,7 +124,7 @@ func (c *Client) GetDeviceManagementScriptByDisplayName(displayName string) (*Re
 
 // CreateDeviceManagementScript creates a new device management script.
 func (c *Client) CreateDeviceManagementScript(request *ResourceDeviceManagementScriptRequest) (*ResourceDeviceManagementScript, error) {
-	endpoint := urideviceManagementScripts
+	endpoint := uriBetaDeviceManagementScripts
 
 	var createdScript ResourceDeviceManagementScript
 	resp, err := c.HTTP.DoRequest("POST", endpoint, request, &createdScript)
@@ -169,7 +142,7 @@ func (c *Client) CreateDeviceManagementScript(request *ResourceDeviceManagementS
 // UpdateDeviceManagementScriptByID updates an existing device management script by its ID.
 // This endpoint supports the HTTP method PATCH for resource attribute updates.
 func (c *Client) UpdateDeviceManagementScriptByID(id string, updateRequest *ResourceDeviceManagementScriptRequest) (*ResourceDeviceManagementScript, error) {
-	endpoint := fmt.Sprintf("%s/%s", urideviceManagementScripts, id)
+	endpoint := fmt.Sprintf("%s/%s", uriBetaDeviceManagementScripts, id)
 
 	var updatedScript ResourceDeviceManagementScript
 	resp, err := c.HTTP.DoRequest("PATCH", endpoint, updateRequest, &updatedScript)
@@ -211,7 +184,7 @@ func (c *Client) UpdateDeviceManagementScriptByDisplayName(displayName string, u
 
 // DeleteDeviceManagementScriptByID deletes an existing device management script by its ID.
 func (c *Client) DeleteDeviceManagementScriptByID(id string) error {
-	endpoint := fmt.Sprintf("%s/%s", urideviceManagementScripts, id)
+	endpoint := fmt.Sprintf("%s/%s", uriBetaDeviceManagementScripts, id)
 
 	resp, err := c.HTTP.DoRequest("DELETE", endpoint, nil, nil)
 	if err != nil {
@@ -233,46 +206,4 @@ func (c *Client) DeleteDeviceManagementScriptByDisplayName(displayName string) e
 	}
 
 	return c.DeleteDeviceManagementScriptByID(script.ID)
-}
-
-// AssignDeviceManagementScriptByGroupID assigns a device management script to a group.
-func (c *Client) AssignDeviceManagementScriptByGroupID(scriptID, groupID string) error {
-	endpoint := fmt.Sprintf("%s/%s/assign", urideviceManagementScripts, scriptID)
-
-	// Construct the request body
-	assignment := struct {
-		DeviceManagementScriptGroupAssignments []struct {
-			OdataType     string `json:"@odata.type"`
-			ID            string `json:"id"`
-			TargetGroupID string `json:"targetGroupId"`
-		} `json:"deviceManagementScriptGroupAssignments"`
-	}{
-		DeviceManagementScriptGroupAssignments: []struct {
-			OdataType     string `json:"@odata.type"`
-			ID            string `json:"id"`
-			TargetGroupID string `json:"targetGroupId"`
-		}{
-			{
-				OdataType:     "#microsoft.graph.deviceManagementScriptGroupAssignment",
-				ID:            "assignment-" + groupID,
-				TargetGroupID: groupID,
-			},
-		},
-	}
-
-	requestData, err := json.Marshal(assignment)
-	if err != nil {
-		return fmt.Errorf(shared.ErrorMsgFailedJsonMarshal, "device management script assignment", err)
-	}
-
-	resp, err := c.HTTP.DoRequest("POST", endpoint, bytes.NewBuffer(requestData), nil)
-	if err != nil {
-		return fmt.Errorf(shared.ErrorMsgFailedCreate, "device management script assignment", err)
-	}
-
-	if resp != nil && resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	return nil
 }
