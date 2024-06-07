5c9d1e5e830936c508c896741859fb65739b6e01
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-06-07 18:08:28 +0100

    Refactor EntityType and NavigationProperty structs in schema.go

--- schema/v2/schema.go
+++ schema/v2/schema.go
@@ -12,19 +12,21 @@ import (
 	"text/template"
 )
 
-// Edmx represents the root element of the CSDL file
+// Edmx represents the root element of the CSDL schema
 type Edmx struct {
-	XMLName     xml.Name     `xml:"Edmx"`
-	Schemas     []Schema     `xml:"DataServices>Schema"`
-	Annotations []Annotation `xml:"Annotations>Annotation"`
+	XMLName xml.Name `xml:"Edmx"`
+	Schemas []Schema `xml:"DataServices>Schema"`
 }
 
-// Schema represents a schema in the CSDL file
+// Schema represents a schema in the CSDL schema
 type Schema struct {
 	XMLName      xml.Name      `xml:"Schema"`
+	Namespace    string        `xml:"Namespace,attr"`
+	Alias        string        `xml:"Alias,attr"`
 	EntityTypes  []EntityType  `xml:"EntityType"`
 	ComplexTypes []ComplexType `xml:"ComplexType"`
 	EnumTypes    []EnumType    `xml:"EnumType"`
+	Annotations  []Annotation  `xml:"Annotations>Annotation"`
 }
 
 // EntityType represents an entity type in the CSDL schema
@@ -93,7 +95,8 @@ type EnumMember struct {
 type Annotation struct {
 	Target      string                     `xml:"Target,attr"`
 	Term        string                     `xml:"Term,attr"`
-	StringValue string                     `xml:"String,attr"`
+	StringValue string                     `xml:"String,attr,omitempty"`
+	BoolValue   bool                       `xml:"Bool,attr,omitempty"`
 	Collection  []AnnotationCollectionItem `xml:"Collection>Record"`
 }
 
@@ -146,120 +149,204 @@ const (
 `
 
 func main() {
-	// Define flags for input and output file paths
-	inputPath := flag.String("input", "schema.csdl", "Path to the CSDL file")
-	outputPath := flag.String("output", "output/output.go", "Path to save the generated Go structs")
-	flag.Parse()
-
+	inputPath, outputPath := parseFlags()
 	log.Println("Starting the CSDL processing...")
 
-	// Read the CSDL file
-	data, err := os.ReadFile(*inputPath)
-	if err != nil {
-		log.Fatalf("Error reading input file: %v", err)
-	}
-	log.Println("Successfully read the input file.")
+	data := readFile(*inputPath)
+	edmx := unmarshalXML(data)
 
-	var edmx Edmx
-
-	// Unmarshal the XML into the Edmx struct
-	err = xml.Unmarshal(data, &edmx)
-	if err != nil {
-		log.Fatalf("Error unmarshalling XML: %v", err)
-	}
-	log.Println("Successfully unmarshalled the XML data.")
-
-	// Ensure the output directory exists
-	outputDir := filepath.Dir(*outputPath)
-	err = os.MkdirAll(outputDir, os.ModePerm)
-	if err != nil {
-		log.Fatalf("Error creating output directory: %v", err)
-	}
-	log.Println("Output directory ensured.")
-
-	// Open the output file
-	outputFile, err := os.Create(*outputPath)
-	if err != nil {
-		log.Fatalf("Error creating output file: %v", err)
-	}
+	ensureOutputDir(*outputPath)
+	outputFile := createOutputFile(*outputPath)
 	defer outputFile.Close()
 
-	// Write the package name at the top of the output file
-	fmt.Fprintln(outputFile, "package graphmodels")
-	fmt.Fprintln(outputFile)
-	fmt.Fprintln(outputFile, "import \"time\"")
-	fmt.Fprintln(outputFile)
-
-	// Map to track generated structs to avoid duplication
+	writePackageHeader(outputFile)
 	generatedStructs := make(map[string]bool)
 
-	// Generate Go structs from the CSDL schemas
-	log.Println("Generating Go structs...")
-	for _, schema := range edmx.Schemas {
-		// Process EntityTypes
-		for _, entityType := range schema.EntityTypes {
-			err := GenerateStruct(outputFile, entityType.Name, entityType.Properties, entityType.NavigationProperties, entityType.Annotations, edmx.Annotations, generatedStructs)
-			if err != nil {
-				log.Fatalf("Error generating struct: %v", err)
-			}
-			log.Printf("Generated struct for %s\n", entityType.Name)
-		}
-
-		// Process ComplexTypes
-		for _, complexType := range schema.ComplexTypes {
-			err := GenerateStruct(outputFile, complexType.Name, complexType.Properties, nil, complexType.Annotations, edmx.Annotations, generatedStructs)
-			if err != nil {
-				log.Fatalf("Error generating struct: %v", err)
-			}
-			log.Printf("Generated struct for %s\n", complexType.Name)
-		}
-	}
-
-	// Generate Go enums from the CSDL schemas
-	log.Println("Generating Go enums...")
-	for _, schema := range edmx.Schemas {
-		for _, enumType := range schema.EnumTypes {
-			err := GenerateEnum(outputFile, enumType, edmx.Annotations)
-			if err != nil {
-				log.Fatalf("Error generating enum: %v", err)
-			}
-			log.Printf("Generated enum for %s\n", enumType.Name)
-		}
-	}
+	generateGoStructs(edmx, outputFile, generatedStructs)
+	generateGoEnums(edmx, outputFile)
 
 	log.Printf("Go structs and enums generated and saved to %s\n", *outputPath)
 }
 
-// GenerateStruct generates a Go struct from a complex type or entity type
-func GenerateStruct(outputFile *os.File, structName string, properties []Property, navigationProperties []NavigationProperty, localAnnotations []Annotation, globalAnnotations []Annotation, generatedStructs map[string]bool) error {
+func parseFlags() (*string, *string) {
+	inputPath := flag.String("input", "schema.csdl", "Path to the CSDL file")
+	outputPath := flag.String("output", "output/output.go", "Path to save the generated Go structs")
+	flag.Parse()
+	return inputPath, outputPath
+}
+
+func readFile(path string) []byte {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Error reading input file: %v", err)
+	}
+	log.Println("Successfully read the input file.")
+	return data
+}
+
+func unmarshalXML(data []byte) Edmx {
+	var edmx Edmx
+	err := xml.Unmarshal(data, &edmx)
+	if err != nil {
+		log.Fatalf("Error unmarshalling XML: %v", err)
+	}
+
+	// Adding detailed logs for the unmarshalling process
+	log.Println("Successfully unmarshalled the XML data.")
+	log.Printf("Edmx Version: %s", edmx.XMLName.Space)
+	log.Printf("Number of Schemas: %d", len(edmx.Schemas))
+	for i, schema := range edmx.Schemas {
+		log.Printf("Schema %d: Namespace: %s, Alias: %s", i, schema.Namespace, schema.Alias)
+		log.Printf("Number of EntityTypes: %d", len(schema.EntityTypes))
+		for j, entityType := range schema.EntityTypes {
+			log.Printf("EntityType %d: Name: %s, BaseType: %s", j, entityType.Name, entityType.BaseType)
+			log.Printf("Number of Properties: %d", len(entityType.Properties))
+			for k, property := range entityType.Properties {
+				log.Printf("Property %d: Name: %s, Type: %s, Nullable: %t", k, property.Name, property.Type, property.Nullable)
+				for l, annotation := range property.Annotations {
+					log.Printf("Property %d, Annotation %d: Term: %s, StringValue: %s, Target: %s", k, l, annotation.Term, annotation.StringValue, annotation.Target)
+				}
+			}
+			log.Printf("Number of NavigationProperties: %d", len(entityType.NavigationProperties))
+			for k, navProp := range entityType.NavigationProperties {
+				log.Printf("NavigationProperty %d: Name: %s, Type: %s", k, navProp.Name, navProp.Type)
+				for l, annotation := range navProp.Annotations {
+					log.Printf("NavigationProperty %d, Annotation %d: Term: %s, StringValue: %s, Target: %s", k, l, annotation.Term, annotation.StringValue, annotation.Target)
+				}
+			}
+			log.Printf("Number of Annotations on EntityType: %d", len(entityType.Annotations))
+			for k, annotation := range entityType.Annotations {
+				log.Printf("EntityType Annotation %d: Term: %s, StringValue: %s, Target: %s", k, annotation.Term, annotation.StringValue, annotation.Target)
+			}
+		}
+		log.Printf("Number of ComplexTypes: %d", len(schema.ComplexTypes))
+		for j, complexType := range schema.ComplexTypes {
+			log.Printf("ComplexType %d: Name: %s", j, complexType.Name)
+			log.Printf("Number of Properties: %d", len(complexType.Properties))
+			for k, property := range complexType.Properties {
+				log.Printf("Property %d: Name: %s, Type: %s, Nullable: %t", k, property.Name, property.Type, property.Nullable)
+				for l, annotation := range property.Annotations {
+					log.Printf("Property %d, Annotation %d: Term: %s, StringValue: %s, Target: %s", k, l, annotation.Term, annotation.StringValue, annotation.Target)
+				}
+			}
+			log.Printf("Number of Annotations on ComplexType: %d", len(complexType.Annotations))
+			for k, annotation := range complexType.Annotations {
+				log.Printf("ComplexType Annotation %d: Term: %s, StringValue: %s, Target: %s", k, annotation.Term, annotation.StringValue, annotation.Target)
+			}
+		}
+		log.Printf("Number of EnumTypes: %d", len(schema.EnumTypes))
+		for j, enumType := range schema.EnumTypes {
+			log.Printf("EnumType %d: Name: %s", j, enumType.Name)
+			log.Printf("Number of Members: %d", len(enumType.Members))
+			for k, member := range enumType.Members {
+				log.Printf("Member %d: Name: %s, Value: %s", k, member.Name, member.Value)
+				for l, annotation := range member.Annotations {
+					log.Printf("Member %d, Annotation %d: Term: %s, StringValue: %s, Target: %s", k, l, annotation.Term, annotation.StringValue, annotation.Target)
+				}
+			}
+			log.Printf("Number of Annotations on EnumType: %d", len(enumType.Annotations))
+			for k, annotation := range enumType.Annotations {
+				log.Printf("EnumType Annotation %d: Term: %s, StringValue: %s, Target: %s", k, annotation.Term, annotation.StringValue, annotation.Target)
+			}
+		}
+		log.Printf("Number of Annotations on Schema: %d", len(schema.Annotations))
+		for k, annotation := range schema.Annotations {
+			log.Printf("Schema Annotation %d: Target: %s, Term: %s, StringValue: %s", k, annotation.Target, annotation.Term, annotation.StringValue)
+		}
+	}
+
+	return edmx
+}
+
+func ensureOutputDir(path string) {
+	outputDir := filepath.Dir(path)
+	err := os.MkdirAll(outputDir, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Error creating output directory: %v", err)
+	}
+	log.Println("Output directory ensured.")
+}
+
+func createOutputFile(path string) *os.File {
+	outputFile, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("Error creating output file: %v", err)
+	}
+	return outputFile
+}
+
+func writePackageHeader(outputFile *os.File) {
+	fmt.Fprintln(outputFile, "package graphmodels")
+	fmt.Fprintln(outputFile)
+	fmt.Fprintln(outputFile, "import \"time\"")
+	fmt.Fprintln(outputFile)
+}
+
+func generateGoStructs(edmx Edmx, outputFile *os.File, generatedStructs map[string]bool) {
+	log.Println("Generating Go structs...")
+	for _, schema := range edmx.Schemas {
+		for _, entityType := range schema.EntityTypes {
+			generateStruct(outputFile, entityType.Name, entityType.Properties, entityType.NavigationProperties, entityType.Annotations, schema.Annotations, generatedStructs)
+		}
+		for _, complexType := range schema.ComplexTypes {
+			generateStruct(outputFile, complexType.Name, complexType.Properties, nil, complexType.Annotations, schema.Annotations, generatedStructs)
+		}
+	}
+}
+
+func generateStruct(outputFile *os.File, structName string, properties []Property, navigationProperties []NavigationProperty, localAnnotations []Annotation, globalAnnotations []Annotation, generatedStructs map[string]bool) {
 	if generatedStructs[structName] {
-		return nil
+		return
 	}
 	generatedStructs[structName] = true
 
-	// Collect all relevant annotations
-	annotations := append(localAnnotations, findGlobalAnnotations(structName, globalAnnotations)...)
+	entityTarget := fmt.Sprintf("microsoft.graph.%s", structName)
+	annotations := collectAnnotations(entityTarget, localAnnotations, globalAnnotations)
 	log.Printf("Collected %d annotations for struct %s", len(annotations), structName)
 
-	tmpl, err := template.New("struct").Parse(StructTemplate)
-	if err != nil {
-		return fmt.Errorf("error parsing struct template: %w", err)
-	}
+	data := prepareStructData(structName, properties, navigationProperties, annotations, globalAnnotations)
+	tmpl := parseTemplate(StructTemplate)
 
-	var buf bytes.Buffer
+	executeTemplate(tmpl, data, outputFile)
+	log.Printf("Successfully generated struct for %s", structName)
+}
+
+func collectAnnotations(target string, localAnnotations, globalAnnotations []Annotation) []Annotation {
+	var result []Annotation
+	for _, annotation := range localAnnotations {
+		if annotation.Target == target {
+			result = append(result, annotation)
+		}
+	}
+	for _, annotation := range globalAnnotations {
+		if annotation.Target == target {
+			result = append(result, annotation)
+		}
+	}
+	return result
+}
+
+func prepareStructData(structName string, properties []Property, navigationProperties []NavigationProperty, annotations, globalAnnotations []Annotation) struct {
+	Name       string
+	Properties []struct {
+		Name, Type, JSONName string
+		Annotations          []Annotation
+	}
+	NavigationProperties []struct {
+		Name, Type, JSONName string
+		Annotations          []Annotation
+	}
+	Annotations []Annotation
+} {
 	data := struct {
 		Name       string
 		Properties []struct {
-			Name        string
-			Type        string
-			JSONName    string
-			Annotations []Annotation
+			Name, Type, JSONName string
+			Annotations          []Annotation
 		}
 		NavigationProperties []struct {
-			Name        string
-			Type        string
-			JSONName    string
-			Annotations []Annotation
+			Name, Type, JSONName string
+			Annotations          []Annotation
 		}
 		Annotations []Annotation
 	}{
@@ -269,7 +356,8 @@ func GenerateStruct(outputFile *os.File, structName string, properties []Propert
 
 	for _, prop := range properties {
 		goType, _ := mapType(prop.Type)
-		propAnnotations := append(prop.Annotations, findGlobalAnnotations(structName+"/"+prop.Name, globalAnnotations)...)
+		propTarget := fmt.Sprintf("microsoft.graph.%s/%s", structName, prop.Name)
+		propAnnotations := collectAnnotations(propTarget, prop.Annotations, globalAnnotations)
 		log.Printf("Property: %s, Type: %s, Annotations: %v", prop.Name, goType, propAnnotations)
 		data.Properties = append(data.Properties, struct {
 			Name        string
@@ -286,7 +374,8 @@ func GenerateStruct(outputFile *os.File, structName string, properties []Propert
 
 	for _, navProp := range navigationProperties {
 		goType, _ := mapType(navProp.Type)
-		navPropAnnotations := append(navProp.Annotations, findGlobalAnnotations(structName+"/"+navProp.Name, globalAnnotations)...)
+		navPropTarget := fmt.Sprintf("microsoft.graph.%s/%s", structName, navProp.Name)
+		navPropAnnotations := collectAnnotations(navPropTarget, navProp.Annotations, globalAnnotations)
 		log.Printf("NavigationProperty: %s, Type: %s, Annotations: %v", navProp.Name, goType, navPropAnnotations)
 		data.NavigationProperties = append(data.NavigationProperties, struct {
 			Name        string
@@ -301,46 +390,72 @@ func GenerateStruct(outputFile *os.File, structName string, properties []Propert
 		})
 	}
 
-	err = tmpl.Execute(&buf, data)
-	if err != nil {
-		return fmt.Errorf("error executing struct template: %w", err)
-	}
-
-	_, err = outputFile.Write(buf.Bytes())
-	if err != nil {
-		return fmt.Errorf("error writing to output file: %w", err)
-	}
-
-	log.Printf("Successfully generated struct for %s", structName)
-	return nil
+	return data
 }
 
-// GenerateEnum generates a Go enum from an enum type
-func GenerateEnum(outputFile *os.File, enumType EnumType, globalAnnotations []Annotation) error {
-	tmpl, err := template.New("enum").Parse(EnumTemplate)
+func parseTemplate(templateString string) *template.Template {
+	tmpl, err := template.New("template").Parse(templateString)
 	if err != nil {
-		return fmt.Errorf("error parsing enum template: %w", err)
+		log.Fatalf("Error parsing template: %v", err)
 	}
+	return tmpl
+}
 
+func executeTemplate(tmpl *template.Template, data interface{}, outputFile *os.File) {
 	var buf bytes.Buffer
+	err := tmpl.Execute(&buf, data)
+	if err != nil {
+		log.Fatalf("Error executing template: %v", err)
+	}
+	_, err = outputFile.Write(buf.Bytes())
+	if err != nil {
+		log.Fatalf("Error writing to output file: %v", err)
+	}
+}
+
+func generateGoEnums(edmx Edmx, outputFile *os.File) {
+	log.Println("Generating Go enums...")
+	for _, schema := range edmx.Schemas {
+		for _, enumType := range schema.EnumTypes {
+			generateEnum(outputFile, enumType, schema.Annotations)
+		}
+	}
+}
+
+func generateEnum(outputFile *os.File, enumType EnumType, globalAnnotations []Annotation) {
+	tmpl := parseTemplate(EnumTemplate)
+
+	annotations := findGlobalAnnotations(fmt.Sprintf("microsoft.graph.%s", enumType.Name), globalAnnotations)
+	log.Printf("Generating enum for %s with %d global annotations", enumType.Name, len(annotations))
+
+	data := prepareEnumData(enumType, annotations, globalAnnotations)
+	executeTemplate(tmpl, data, outputFile)
+
+	log.Printf("Successfully generated enum for %s", enumType.Name)
+}
+
+func prepareEnumData(enumType EnumType, annotations, globalAnnotations []Annotation) struct {
+	Name    string
+	Members []struct {
+		Name, Value string
+		Annotations []Annotation
+	}
+	Annotations []Annotation
+} {
 	data := struct {
 		Name    string
 		Members []struct {
-			Name        string
-			Value       string
+			Name, Value string
 			Annotations []Annotation
 		}
 		Annotations []Annotation
 	}{
 		Name:        capitalize(enumType.Name),
-		Annotations: findGlobalAnnotations(enumType.Name, globalAnnotations),
+		Annotations: annotations,
 	}
 
-	log.Printf("Generating enum for %s with %d global annotations", enumType.Name, len(data.Annotations))
-
 	for _, member := range enumType.Members {
-		// Combine global and member-specific annotations
-		memberAnnotations := append(member.Annotations, findGlobalAnnotations(fmt.Sprintf("%s/%s", enumType.Name, member.Name), globalAnnotations)...)
+		memberAnnotations := append(member.Annotations, findGlobalAnnotations(fmt.Sprintf("microsoft.graph.%s/%s", enumType.Name, member.Name), globalAnnotations)...)
 		log.Printf("EnumMember: %s, Value: %s, Annotations: %v", member.Name, member.Value, memberAnnotations)
 
 		data.Members = append(data.Members, struct {
@@ -354,21 +469,9 @@ func GenerateEnum(outputFile *os.File, enumType EnumType, globalAnnotations []An
 		})
 	}
 
-	err = tmpl.Execute(&buf, data)
-	if err != nil {
-		return fmt.Errorf("error executing enum template: %w", err)
-	}
-
-	_, err = outputFile.Write(buf.Bytes())
-	if err != nil {
-		return fmt.Errorf("error writing to output file: %w", err)
-	}
-
-	log.Printf("Successfully generated enum for %s", enumType.Name)
-	return nil
+	return data
 }
 
-// mapType maps a CSDL type to a Go type and indicates if it's a complex type
 func mapType(csdlType string) (string, bool) {
 	switch csdlType {
 	case "Edm.String":
@@ -405,17 +508,12 @@ func mapType(csdlType string) (string, bool) {
 			goType, isComplex := mapType(elementType)
 			return "[]*" + goType, isComplex
 		}
-
-		// Split the type by '.' to extract the last part (type name)
 		parts := strings.Split(csdlType, ".")
 		typeName := parts[len(parts)-1]
-
-		// Capitalize the type name directly
 		return capitalize(typeName), true
 	}
 }
 
-// capitalize capitalizes the first letter of a string
 func capitalize(s string) string {
 	if len(s) == 0 {
 		return s
@@ -423,16 +521,13 @@ func capitalize(s string) string {
 	return strings.ToUpper(s[:1]) + s[1:]
 }
 
-// findGlobalAnnotations finds global annotations for a given target
 func findGlobalAnnotations(target string, annotations []Annotation) []Annotation {
 	var result []Annotation
 	log.Printf("Finding global annotations for target: %s", target)
 	for _, annotation := range annotations {
-		if annotation.Target == target {
+		if annotation.Target == target || annotation.Target == fmt.Sprintf("microsoft.graph.%s", target) {
 			log.Printf("Found annotation: %s - %s: %s", annotation.Target, annotation.Term, annotation.StringValue)
 			result = append(result, annotation)
-		} else {
-			log.Printf("Annotation target mismatch: annotation.Target=%s, target=%s", annotation.Target, target)
 		}
 	}
 	log.Printf("Total annotations found for target %s: %d", target, len(result))
